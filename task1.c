4c2a146596acee21bf29f08301e926286f5378c0
erokhin.dev <erokhin.dev@mail.ru>
2021-01-24 17:23:59 +0300

    Новая программа (task1).

---
+++ task1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+     int N;
+        printf("Введите число: ");
+        scanf("%d", &N);
+
+    for (int i = 0;i < N;i++){
+        printf("#");
+    }
+            printf("\n");
+
+if (N!=2){
+    for (int i = 0;i < N;i++){
+        printf("#");
+
+        for (int j = 1;j < N-1;j++){
+            printf("*");
+        }
+                printf("#\n");
+    }
+}
+    for (int i = 0;i < N;i++){
+            printf("#");
+    }
+    
+    
+      return 0;
+}
