c64fb5844908bdbb8a8c4ccacab17be7c5cee0dd
erokhin.dev <erokhin.dev@mail.ru>
2021-01-25 22:03:58 +0300

    Вторая задачка (task2.exe)

---
+++ task2.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main(){
+     
+    char i, j, N;
+    printf("ВВедите число: ");
+    scanf("%d", &N);
+
+    for (i = 0;i < N + 1;i++){
+        for (j = 0; j < i; j++)
+            printf("*");
+            for (j = N; j > i; j--)
+                printf(" ");
+        printf("\n");
+    }
+
+        printf("\n");
+    for (i = 0;i < N + 1;i++){
+        for (j = N; j > i; j--)
+            printf("*");
+            for (j = 0; j < i; j++)
+                printf(" ");
+        printf("\n");
+    }
+
+        printf("\n");
+     for (i = 0;i < N;i++){
+        for (j = 0; j < i; j++)
+            printf(" ");
+            for (j = N; j > i; j--)
+                printf("*");
+        printf("\n");
+    }
+ 
+        printf("\n");
+    for (i = 0;i < N + 1;i++){
+        for (j = N; j > i; j--)
+            printf(" ");
+            for (j = 0; j < i; j++)
+                printf("*");
+        printf("\n");
+    }
+          return 0;
+}
