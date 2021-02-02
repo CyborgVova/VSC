5b9a09b5431e84a3f6e313b5566c978fabcafc45
erokhin.dev <erokhin.dev@mail.ru>
2021-02-02 14:53:53 +0300

    Добавлены значки градуса и буквы F & C

--- temp.c
+++ temp.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
+#define EOS (-1)
 
 int main(){
+    FILE *fahr_cel;
+    fahr_cel = fopen("temp.txt", "w");
     float fahr, cel;
     for(fahr = 0; fahr <= 212; fahr++){
         cel = 5.0 / 9.0 * (fahr - 32);
-    printf("%10.f%+10.3f\n", fahr, cel);
+    fprintf(fahr_cel, "%5.f%cF%+10.3f%cC\n", fahr, 176, cel, 176);
     }
+    fclose(fahr_cel);
     return 0;
 }
