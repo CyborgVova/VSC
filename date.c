ee27f00b44516872c979fd7ccdfcc71e82c00f05
erokhin.dev <erokhin.dev@mail.ru>
2021-02-02 23:12:53 +0300

    Время и дата европейского стандарта из одного файла сохраняются в американском стиле в другом файле

---
+++ date.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+int h, min, sec, day, mon, y;
+char *x = "PM";
+FILE *inp, *out;
+inp = fopen("input.txt", "r");
+out = fopen("output.txt", "w");
+fscanf(inp, "%d:%d:%d %d.%d.%d", &h, &min, &sec, &day, &mon, &y);
+if(h < 12) x = "AM";
+if(h > 12) h = h%12;
+ fprintf(out, "%d:%d:%d %s %d/%d/%d", h, min, sec, x, mon, day, y);
+    fclose(inp);
+    fclose(out);
+    return 0;
+}
