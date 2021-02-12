a30abf7855eb630b674b9b2328e67710d1ae846f
erokhin.dev <erokhin.dev@mail.ru>
2021-02-13 01:36:47 +0300

    Изменена функция void на int для возврата значения в функцию main

--- Masstring1.c
+++ Masstring1.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 #include<string.h>
 
-void countChar(const char *string);
+int countChar(const char *string);
 
 int main(){
     char str[100];
     printf("Input any string: ");
     fgets(str, sizeof(str), stdin);
     fflush(stdin);
-    countChar(str);
+    int res = countChar(str);
+    printf("%d", res - 1);
     return 0;
 }
 
-void countChar(const char *string){
+int countChar(const char *string){
     int i = 0;
     while(string[i] != '\0'){
         i++;
     }
-    printf("%d characters", i - 1);
+    return i;
 }
